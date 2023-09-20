d638a4ed7ebebf6777a41c225a224df579f1588b
tqminh0907 <tqminh.0907@gmail.com>
2023-09-20 14:11:13 +0700

    add model

---
+++ Models/Domain/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace Booking_Room.Models.Domain
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public bool IsBooked { get; set; }
+        public RoomType RoomType { get; set; }
+    }
+}
---
+++ Models/Domain/RoomType.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking_Room.Models.Domain
+{
+    public class RoomType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Room> Rooms { get; set; }
+    }
+}
