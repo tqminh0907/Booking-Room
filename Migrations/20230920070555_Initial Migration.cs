881200580144c407f8fb7e15c2ced73fb2633d65
tqminh0907 <tqminh.0907@gmail.com>
2023-09-20 14:12:24 +0700

    add migration create database

---
+++ Migrations/20230920070555_Initial Migration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking_Room.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "RoomTypes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Rooms");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "RoomTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
