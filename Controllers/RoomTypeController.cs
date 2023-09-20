5b4aa97d0963540d86756ce5ea26b377791e5b15
tqminh0907 <tqminh.0907@gmail.com>
2023-09-20 14:13:55 +0700

    build controller and view manager roomtype

---
+++ Controllers/RoomTypeController.cs
@@ -0,0 +1,42 @@
+﻿using Booking_Room.Models.Domain;
+using Booking_Room.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking_Room.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        private readonly DBContext dbContext;
+
+        public RoomTypeController(DBContext dbContext) 
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var roomTypes = dbContext.RoomTypes.ToList();
+            return View(roomTypes);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitAdd()
+        {
+            string name = Request.Form["name"];
+
+            var roomtype = new RoomType{Name = name};
+
+            dbContext.RoomTypes.Add(roomtype);
+            dbContext.SaveChanges();
+
+            return View("Index");
+        }
+    }
+}
